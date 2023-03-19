a6e32b84e78db9a417772917f444740f60982e22
DhanushM12 <dhanushmukhi@gmail.com>
2023-03-19 16:06:10 +0530

    Added the generation code of jwt

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken')
 const port = 8080;
 
 app.get('/', function (req, res) {
     res.json({message : "Welcome to node js batch Aug25"})
     // res.send('<h1>Welcome to node js!!!!</h1>')
 })
-  
+
+app.post('/tokenGenerate', (req, res) => {
+    const user = {
+        id: 1234,
+        username: 'Aug25B2022',
+        email: 'aug25B@coding.com'
+    }
+    jwt.sign(user, 'secret', function(err, token) {
+        if(err){
+            res.sendStatus(403)
+        }
+        else{
+            res.json({
+                token
+            })
+        }
+      });
+})
 
 app.listen(port, function(err){
     if(err){
