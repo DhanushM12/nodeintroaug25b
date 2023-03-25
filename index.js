a8601d7a0e9b05631a3eac368301bd5a1221c453
DhanushM12 <dhanushmukhi@gmail.com>
2023-03-25 13:07:45 +0530

    verify token

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.post('/tokenGenerate', (req, res) => {
         username: 'Aug25B2022',
         email: 'aug25B@coding.com'
     }
-    jwt.sign(user, 'secret', function(err, token) {
+    jwt.sign(user, 'secret', {expiresIn: '60s'}, function(err, token) {
         if(err){
             res.sendStatus(403)
         }
@@ -26,6 +26,29 @@ app.post('/tokenGenerate', (req, res) => {
       });
 })
 
+app.post('/verifyToken', extractToken, (req, res) => {
+    jwt.verify(req.token, 'secret', function(err, data) {
+        if(err){
+            res.sendStatus(403)
+        }
+        else{
+            res.json({ message: 'User access granted', data})
+        }
+      });
+})
+
+function extractToken(req, res, next){
+    const bearerHeader = req.headers['authorization']; // "bearer token"
+    if(bearerHeader !== undefined){
+        const bearer = bearerHeader.split(" "); // ["bearer", "token"]
+        const bearerToken = bearer[1]; // token
+        req.token = bearerToken;
+        next();
+    }
+    else{
+        res.sendStatus(403)
+    }
+}
 app.listen(port, function(err){
     if(err){
         console.log(`Error in starting the server : ${err}`)
